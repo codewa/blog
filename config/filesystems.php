930c5f22feb5663d46dfbaf01259526bc6babaae
xiaohaihe <xiaohaihe@drys.com.cn>
2016-12-19 01:38:20 +0800

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -2,44 +2,44 @@
 
 return [
 
-        /*
-        |--------------------------------------------------------------------------
-        | Default Filesystem Disk
-        |--------------------------------------------------------------------------
-        |
-        | Here you may specify the default filesystem disk that should be used
-        | by the framework. A "local" driver, as well as a variety of cloud
-        | based drivers are available for your choosing. Just store away!
-        |
-        | Supported: "local", "ftp", "s3", "rackspace"
-        |
-        */
+    /*
+    |--------------------------------------------------------------------------
+    | Default Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default filesystem disk that should be used
+    | by the framework. A "local" driver, as well as a variety of cloud
+    | based drivers are available for your choosing. Just store away!
+    |
+    | Supported: "local", "ftp", "s3", "rackspace"
+    |
+    */
 
     'default' => 'local',
 
-        /*
-        |--------------------------------------------------------------------------
-        | Default Cloud Filesystem Disk
-        |--------------------------------------------------------------------------
-        |
-        | Many applications store files both locally and in the cloud. For this
-        | reason, you may specify a default "cloud" driver here. This driver
-        | will be bound as the Cloud disk implementation in the container.
-        |
-        */
+    /*
+    |--------------------------------------------------------------------------
+    | Default Cloud Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Many applications store files both locally and in the cloud. For this
+    | reason, you may specify a default "cloud" driver here. This driver
+    | will be bound as the Cloud disk implementation in the container.
+    |
+    */
 
     'cloud' => 's3',
 
-        /*
-        |--------------------------------------------------------------------------
-        | Filesystem Disks
-        |--------------------------------------------------------------------------
-        |
-        | Here you may configure as many filesystem "disks" as you wish, and you
-        | may even configure multiple disks of the same driver. Defaults have
-        | been setup for each driver as an example of the required options.
-        |
-        */
+    /*
+    |--------------------------------------------------------------------------
+    | Filesystem Disks
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure as many filesystem "disks" as you wish, and you
+    | may even configure multiple disks of the same driver. Defaults have
+    | been setup for each driver as an example of the required options.
+    |
+    */
 
     'disks' => [
 
@@ -61,17 +61,13 @@ return [
             'region' => 'your-region',
             'bucket' => 'your-bucket',
         ],
-        'qiniu' => [
-            'driver'  => 'qiniu',
-            'domains' => [
-                'default'   => env('QINIU_DOMAINS_DEFAULT', ''), //你的七牛域名
-                'https'     => env('QINIU_DOMAINS_HTTPS', ''),         //你的HTTPS域名
-                'custom'    => env('QINIU_DOMAINS_CUSTOM', ''),     //你的自定义域名
-            ],
-            'access_key'=> env('QINIU_AXXESS_KEY', ''),  //AccessKey
-            'secret_key'=> env('QINIU_SECRET_KEY', ''),  //SecretKey
-            'bucket'    => env('QINIU_BUCKET', ''),  //Bucket名字
-            'notify_url'=> env('QINIU_NOTIFY_URL', ''),  //持久化处理回调地址
+       'qiniu' => [
+            'driver'        => 'qiniu',
+            'domain'        => '',//你的七牛域名
+            'access_key'    => '',//AccessKey
+            'secret_key'    => '',//SecretKey
+            'bucket'        => '',//Bucket名字
+            'transport'     => 'http',//如果支持https，请填写https，如果不支持请填写http
         ],
 
         'upyun' => [
