29ed497a9ee25e9b8c83732c72b2e2ad99a4eb56
xiaohaihe <xiaohaihe@drys.com.cn>
2016-12-19 00:57:27 +0800

    update

--- app/Repositories/admin/ArticleRepository.php
+++ app/Repositories/admin/ArticleRepository.php
@@ -253,7 +253,7 @@ class ArticleRepository
 //		$disk = QiniuStorage::disk('qiniu');
 		$disk = \Storage::drive('upyun');
 		$fileName = md5($file->getClientOriginalName().time().rand()).'.'.$file->getClientOriginalExtension();
-		$bool = $disk->write(config('admin.global.imagePath').$fileName,file_get_contents($file->getRealPath()));
+		$bool = $disk->write(config('admin.global.imagePath').$fileName,fopen($file->getRealPath()));
 		if ($bool) {
 			$path = $disk->read(config('admin.global.imagePath').$fileName);
 			return $path;
