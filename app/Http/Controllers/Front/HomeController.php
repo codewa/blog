48a35dab18d20a8203b182a1c84498502b9954b0
xiaohaihe <xiaohaihe@drys.com.cn>
2017-03-04 02:30:49 +0800

    update

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -4,12 +4,14 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use FrontRepository;
+use Illuminate\Http\Response;
 class HomeController extends Controller
 {
     public function index()
     {
     	$articles = FrontRepository::getArticles();
     	$cate = FrontRepository::getAllCategory();
+
     	return view('front.home.index')->with(compact(['articles','cate']));
 
     }
