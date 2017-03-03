1bfb889479ea884991595a3f1cb32c40203f22f9
xiaohaihe <xiaohaihe@drys.com.cn>
2017-03-04 02:37:16 +0800

    update

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -13,7 +13,12 @@ class HomeController extends Controller
     	$cate = FrontRepository::getAllCategory();
 		$offset = 86400; //1天
 		$expire = gmdate("D, d M Y H:i:s", time() + $offset) . " GMT";
-		return response()->view('haopinjia.www.index');
+		$returnnData['articles'] = $articles;
+		$returnData['cate'] = $cate;
+		return Response::view('front.home.index', $returnData)->header('Cache-Control', 'public')
+				->header('Content-Type', 'text/html; charset=utf-8')
+				->header('Expires',$expire);
+//    	return view('front.home.index')->with(compact(['articles','cate']));
 
     }
 }
