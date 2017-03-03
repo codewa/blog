9e7d95d9dc759083137d20cd2fcc614a8d331b2a
xiaohaihe <xiaohaihe@drys.com.cn>
2017-03-04 02:41:37 +0800

    up

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use FrontRepository;
-use Illuminate\Http\Response;
 class HomeController extends Controller
 {
     public function index()
@@ -15,7 +14,6 @@ class HomeController extends Controller
 		$offset = 86400 * 3;
 		$expire = "Expires: " . gmdate("D, d M Y H:i:s", time() + $offset) . " GMT";
 		header($expire);
-
 	  	return view('front.home.index')->with(compact(['articles','cate']));
 
     }
