c619b7c2fc9de324b7f06cd3e0cf018c37ab7f9f
xiaohaihe <xiaohaihe@drys.com.cn>
2017-03-04 02:16:42 +0800

    update

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -4,18 +4,13 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use FrontRepository;
-use Redis;
 class HomeController extends Controller
 {
     public function index()
     {
     	$articles = FrontRepository::getArticles();
     	$cate = FrontRepository::getAllCategory();
-		$offset = 10; //1天
-		$expire = gmdate("D, d M Y H:i:s", time() + $offset) . " GMT";
-		header('Cache-Control', 'public');
-		header('Content-Type', 'text/html; charset=utf-8');
-		header('Expires',$expire);
+		dd($articles);
     	return view('front.home.index')->with(compact(['articles','cate']));
 
     }
